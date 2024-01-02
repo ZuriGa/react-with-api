7c5a5af5f457b7b668ca3479c678cc9be18db8bd
ZuriGa <zurisadailopezmartinez@gmail.com>
2024-01-02 13:52:05 -0800

    1-3 test passed

---
+++ src/__test__/reducers/top-stories-reducer.test.js
@@ -0,0 +1,34 @@
+import topStoriesReducer from '../../reducers/top-stories-reducer';
+import * as c from '../../actions/ActionTypes';
+
+describe('topStoriesReducer', () => {
+
+    let action;
+
+    const initialState = {
+        isLoaded: false,
+        topStories: [],
+        error: null
+    };
+
+    test('Should successfully throw a new error if a non-matching action type is passed into it', () => {
+        expect(
+            () => {
+                topStoriesReducer(initialState, {type: null })
+            }
+        ).toThrowError("There is no action matching null.");
+    });
+
+    test('Successfully getting top stories should change isLoaded to true and update topStories', () => {
+        const topStories = "An article";
+        action = {
+            type: c.GET_TOP_STORIES_SUCCESS,
+            topStories
+        };
+        expect(topStoriesReducer(initialState, action)).toEqual({
+            isLoaded: true,
+            topStories: "An article",
+            error: null
+        });
+    });
+});
